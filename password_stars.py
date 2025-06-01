931c210b50bc57e7addb6672385e4f2d6d142a7d
jd115410 <erin.massingham@my.jcu.edu.au>
2025-06-01 22:43:52 +1000

    Add password check program

--- password_stars.py
+++
@@ -1,8 +0,0 @@
-min_length = 8
-
-password = input("Password: ")
-while len(password) < min_length:
-    print(f"Error: Password must be at least {min_length} characters.")
-    password = input("Password: ")
-
-print("*" * len(password))
